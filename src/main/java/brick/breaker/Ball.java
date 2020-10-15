b7e277c2e98d6bc27b57878961e0769d044fec86
Justin Ho <justinhodev@gmail.com>
2020-10-14 21:58:10 -0700

    feat(ball): add coordinate field
    cartesian coordinate system getter and setter with processing pvector

--- src/main/java/brick/breaker/Ball.java
+++ src/main/java/brick/breaker/Ball.java
@@ -1,6 +1,9 @@
 package brick.breaker;
 
+import processing.core.PVector;
+
 public class Ball {
+  private PVector position;
   private int radius;
 
   /**
@@ -10,6 +13,12 @@ public class Ball {
    */
   public Ball(int size) {
     this.radius = size;
+    this.position = new PVector();
+  }
+
+  public Ball setLocation(PVector newPosition) {
+    this.position = newPosition;
+    return this;
   }
 
   /**
@@ -19,5 +28,9 @@ public class Ball {
    */
   public int getSize() {
     return this.radius;
+  }
+
+  public PVector getLocation() {
+    return this.position;
   }
 }
