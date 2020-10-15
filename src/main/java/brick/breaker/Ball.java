1ec917d67b9fd132fb959bc932590404275ecc63
Justin Ho <justinhodev@gmail.com>
2020-10-14 21:41:31 -0700

    docs(ball): add javadoc blocks for functions

--- src/main/java/brick/breaker/Ball.java
+++ src/main/java/brick/breaker/Ball.java
@@ -3,10 +3,20 @@ package brick.breaker;
 public class Ball {
   private int radius;
 
+  /**
+   * Default constructor to create a {@link Ball} object.
+   *
+   * @param size radius of the {@link Ball}
+   */
   public Ball(int size) {
     this.radius = size;
   }
 
+  /**
+   * Getter for radius of {@link Ball}.
+   *
+   * @return radius of circle
+   */
   public int getSize() {
     return this.radius;
   }
