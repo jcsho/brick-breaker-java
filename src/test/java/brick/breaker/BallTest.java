b8e0eab353bdbb181356620b29c392bd47e38992
Justin Ho <justinhodev@gmail.com>
2020-10-14 21:50:08 -0700

    test(ball): create test case for coordinates
    2d cartesian coordinates stored as floating point numbers

--- src/test/java/brick/breaker/BallTest.java
+++ src/test/java/brick/breaker/BallTest.java
@@ -3,6 +3,7 @@ package brick.breaker;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import processing.core.PVector;
 
 public class BallTest {
 
@@ -11,6 +12,16 @@ public class BallTest {
     int size = 5;
     Ball ball = new Ball(size);
     assertEquals(size, ball.getSize());
+  }
+
+  @Test
+  public void testBallSetStartingLocation() {
+    float x = 50;
+    float y = 50;
+    PVector location = new PVector(x, y);
+    Ball ball = new Ball(50);
+    ball.setLocation(location);
+    assertEquals(location, ball.getLocation());
   }
 
 }
