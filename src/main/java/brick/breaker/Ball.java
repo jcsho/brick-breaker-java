91c8bfa6835e2b04230f0e4ece5ba1da27054338
Justin Ho <justinhodev@gmail.com>
2020-10-14 21:05:25 -0700

    feat(ball): create class and add default constructor for size

---
+++ src/main/java/brick/breaker/Ball.java
@@ -0,0 +1,13 @@
+package brick.breaker;
+
+public class Ball {
+  private int radius;
+
+  public Ball(int size) {
+    this.radius = size;
+  }
+
+  public int getSize() {
+    return this.radius;
+  }
+}
