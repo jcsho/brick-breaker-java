cabc8e7e1d1aeadf64d3c823d0eeefc8bb8a6d5a
Justin Ho <justinhodev@gmail.com>
2020-10-18 22:46:11 -0700

    test(ball): add collision test
    places brick object within bounds of ball, function should return true, refactor test class to use
    static instances

--- src/test/java/brick/breaker/BallTest.java
+++ src/test/java/brick/breaker/BallTest.java
@@ -1,26 +1,43 @@
 package brick.breaker;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import processing.core.PVector;
 
 public class BallTest {
 
+  private static Ball ball = new Ball();
+  private static PVector ballPosition = new PVector(100, 100);
+  private static PVector ballSize = new PVector(50, 50);
+
   @Test
   public void testBallSetSize() {
-    PVector size = new PVector(50, 50);
-    Ball ball = new Ball().setSize(size);
-    assertEquals(size, ball.getSize());
+    ball.setSize(ballSize);
+    assertEquals(ballSize.x, ball.getSize().x, 0.1);
+    assertEquals(ballSize.y, ball.getSize().y, 0.1);
   }
 
   @Test
   public void testBallSetStartingLocation() {
-    float x = 50;
-    float y = 50;
-    PVector location = new PVector(x, y);
-    Ball ball = new Ball().setSize(new PVector(50, 50)).setPosition(location);
-    assertEquals(location, ball.getPosition());
+    ball.setPosition(ballPosition);
+    assertEquals(ballPosition.x, ball.getPosition().x, 0.1);
+    assertEquals(ballPosition.y, ball.getPosition().y, 0.1);
+  }
+
+  @Test
+  public void testBallDetectCollisionTrue() {
+    ball.setPosition(ballPosition).setSize(ballSize);
+
+    // collision should happen at brick's bottom left corner
+    // and balls' top right vertex (approx (113, 121))
+    PVector brickSize = new PVector(100, 50);
+    PVector brickPosition = new PVector(160, 140);
+    Brick brick = new Brick().setPosition(brickPosition).setSize(brickSize);
+
+    boolean isCollidingWithBrick = ball.isColliding(brick);
+    assertTrue(isCollidingWithBrick);
   }
 
 }
