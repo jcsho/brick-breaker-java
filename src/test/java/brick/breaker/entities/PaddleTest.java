3b0b256a3dcc833b31685f6ca28ada8a2552f78b
Justin Ho <justinhodev@gmail.com>
2020-10-24 23:28:31 -0700

    test(paddle): test out of bounds movement for target position
    paddle will only move to target position, if target position is within the game window, so should
    the paddle

--- src/test/java/brick/breaker/entities/PaddleTest.java
+++ src/test/java/brick/breaker/entities/PaddleTest.java
@@ -9,14 +9,19 @@ import org.junit.Test;
 import processing.core.PVector;
 
 public class PaddleTest {
+  private static final PVector initialPosition = new PVector(50, 450);
+  private static final PVector minBoundary = new PVector(0, 0);
+  private static final PVector maxBoundary = new PVector(500, 500);
+  private static final PVector paddleSize = new PVector(100, 50);
 
   @Test
   public void testPaddleMovement() {
-    PVector initialPosition = new PVector(500, 800);
     PVector newPosition = new PVector(600, 0);
     float maxSpeed = 5;
 
     Paddle paddle = new Paddle();
+    paddle.setMovementBoundary(minBoundary, maxBoundary);
+    paddle.setSize(paddleSize);
     paddle.setPosition(initialPosition);
     paddle.setMaxSpeed(maxSpeed);
     paddle.setTargetPosition(newPosition);
@@ -33,5 +38,17 @@ public class PaddleTest {
       Paddle paddle = new Paddle();
       paddle.setMaxSpeed(incorrectArgument);
     });
+  }
+
+  @Test
+  public void testSetTargetPositionOutOfBounds() {
+    PVector newPosition = new PVector(-50, initialPosition.y);
+    Paddle paddle = new Paddle();
+    paddle.setMovementBoundary(minBoundary, maxBoundary);
+    paddle.setPosition(initialPosition);
+    paddle.setSize(paddleSize);
+    paddle.setTargetPosition(newPosition);
+    paddle.update();
+    assertThat(paddle.getPosition().array(), IsEqual.equalTo(initialPosition.array()));
   }
 }
