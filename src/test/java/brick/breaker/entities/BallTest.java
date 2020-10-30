1f99f5ef270b0d0020d5cefedf5c19fe3c7c9707
Justin Ho <justinhodev@gmail.com>
2020-10-30 00:14:45 -0700

    test(ball): move test objects instantiation into before each function

--- src/test/java/brick/breaker/entities/BallTest.java
+++ src/test/java/brick/breaker/entities/BallTest.java
@@ -6,35 +6,44 @@ import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import processing.core.PVector;
 
 public class BallTest {
 
-  private static Ball ball = new Ball();
-  private static PVector ballPosition = new PVector(26, 26);
-  private static PVector ballSize = new PVector(50, 50);
-  private static PVector minBoundary = new PVector(0, 0);
-  private static PVector maxBoundary = new PVector(500, 500);
+  private Ball ball;
+  private PVector ballPosition;
+  private PVector ballSize;
+  private PVector minBoundary;
+  private PVector maxBoundary;
+
+  @BeforeEach
+  public void initialize() {
+    ballPosition = new PVector(26, 26);
+    ballSize = new PVector(50, 50);
+    minBoundary = new PVector(0, 0);
+    maxBoundary = new PVector(500, 500);
+    ball = new Ball();
+    ball.setPosition(ballPosition);
+    ball.setSize(ballSize);
+    ball.setMovementBoundary(minBoundary, maxBoundary);
+  }
 
   @Test
   public void testBallSetSize() {
-    ball.setSize(ballSize);
     assertEquals(ballSize.x, ball.getSize().x, 0.1);
     assertEquals(ballSize.y, ball.getSize().y, 0.1);
   }
 
   @Test
   public void testBallSetStartingLocation() {
-    ball.setPosition(ballPosition);
     assertEquals(ballPosition.x, ball.getPosition().x, 0.1);
     assertEquals(ballPosition.y, ball.getPosition().y, 0.1);
   }
 
   @Test
   public void testBallDetectCollisionTrue() {
-    ball.setPosition(ballPosition).setSize(ballSize);
-
     // collision should happen at brick's bottom left corner
     // and balls' top right vertex (approx (113, 121))
     PVector brickSize = new PVector(100, 50);
@@ -51,7 +60,6 @@ public class BallTest {
   public void testBallMovement() {
     PVector newPosition = new PVector(0, 0);
     float speed = 60;
-    ball.setMovementBoundary(minBoundary, maxBoundary);
     ball.setMaxSpeed(speed);
     ball.setTargetPosition(newPosition);
     ball.update();
@@ -61,7 +69,6 @@ public class BallTest {
   @Test
   public void testBallOutOfBounds() {
     PVector newPosition = new PVector(0, 0);
-    ball.setMovementBoundary(minBoundary, maxBoundary);
     ball.setTargetPosition(newPosition);
     for (int i = 0; i < 5; i++) {
       ball.update();
